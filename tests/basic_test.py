22690d2ebcd30c7b0bfeed391a966d4bbc9ae5d0
destin-v <47424506+destin-v@users.noreply.github.com>
2026-01-12 15:04:07 -0500

    Update pytests

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,6 +1,15 @@
 import time
 
+from tests.utils import attach_logger
+from loguru import logger
+
+attach_logger(__file__, "tests")
+
 
 def test_hello_world():
-    print("hello world")
+    logger.debug("hello world")
     time.sleep(1)
+
+if __name__ == "__main__":
+    test_hello_world()
+    
